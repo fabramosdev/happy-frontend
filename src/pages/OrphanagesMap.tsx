3f1998350a0363845b57743fab4de9a733bd83a9
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-10-14 22:49:46 -0300

    Add pages: CreateOrphanage/Orphanage

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
-import { FiPlus, FiSun, FiMoon } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet'
-
-
-import 'leaflet/dist/leaflet.css'
+import { FiPlus, FiSun, FiMoon, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import mapMarkerImg from '../images/map-marker.svg'
 import '../styles/pages/orphanages-map.css'
+import mapIcon from '../utils/mapIcons';
 
 function OrphanagesMap() {
 
@@ -21,7 +19,7 @@ function OrphanagesMap() {
         <div id="page-map">
             <aside className={`${darkMode ? 'dark-sidebar' : 'light'}`}>
                 <header>
-                    <img src={mapMarkerImg} alt="Happy"/>
+                    <img src={mapMarkerImg} alt="Happy" />
 
                     <h2>Escolha um orfanato no mapa</h2>
                     <p>Muitas crianças estão esperando a sua visita :)</p>
@@ -34,22 +32,34 @@ function OrphanagesMap() {
             </aside>
 
             <Map
-                center={[-22.6521336,-43.281157]}
+                center={[-22.6521336, -43.281157]}
                 zoom={15}
                 style={{ width: '100%', height: '100%' }}
             >
-                {/* {<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />} */}
-                <TileLayer 
-                    url={`https://api.mapbox.com/styles/v1/mapbox/${darkMode ? 'dark':'light'}-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/${darkMode ? 'dark' : 'light'}-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+
+
+                <Marker
+                    icon={mapIcon}
+                    position={[-22.6521336, -43.281157]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                        Lar das Meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={32} color="#fff" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
-            
-            {/** DARK MODE MAP */}    
+
+            {/** DARK MODE MAP */}
             <div className="dark-mode" onClick={toggle}>
                 {darkMode ? <FiSun size={32} color="#fff" /> : <FiMoon size={32} color="#fff" />}
             </div>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus color="#fff" size={32} />
             </Link>
         </div>

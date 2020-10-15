09ccccaa0dba1babf5d5c1acd7141ffac23b4206
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-10-15 18:00:55 -0300

    Orphanage details page completed

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { FiPlus, FiSun, FiMoon, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
@@ -6,14 +6,29 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import mapMarkerImg from '../images/map-marker.svg'
 import '../styles/pages/orphanages-map.css'
 import mapIcon from '../utils/mapIcons';
+import api from '../services/api';
+
+interface IOrphanage {
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
 
 function OrphanagesMap() {
 
     const [darkMode, setDarkMode] = useState(false)
+    const [orphanages, setOrphanages] = useState<IOrphanage[]>([])
 
     const toggle = () => {
         setDarkMode(!darkMode)
     }
+
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data);
+        })
+    }, [])
 
     return (
         <div id="page-map">
@@ -40,21 +55,25 @@ function OrphanagesMap() {
                     url={`https://api.mapbox.com/styles/v1/mapbox/${darkMode ? 'dark' : 'light'}-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
 
-
-                <Marker
-                    icon={mapIcon}
-                    position={[-22.6521336, -43.281157]}
-                >
-                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
-                        Lar das Meninas
-                        <Link to="/orphanages/1">
-                            <FiArrowRight size={32} color="#fff" />
-                        </Link>
-                    </Popup>
-                </Marker>
+                {orphanages.map(orphanage => {
+                    return (
+                        <Marker
+                            icon={mapIcon}
+                            position={[orphanage.latitude, orphanage.longitude]}
+                            key={orphanage.id}
+                        >
+                            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                                {orphanage.name}
+                                <Link to={`/orphanages/${orphanage.id}`}>
+                                    <FiArrowRight size={32} color="#fff" />
+                                </Link>
+                            </Popup>
+                        </Marker>
+                    )
+                })}
+                
             </Map>
 
-            {/** DARK MODE MAP */}
             <div className="dark-mode" onClick={toggle}>
                 {darkMode ? <FiSun size={32} color="#fff" /> : <FiMoon size={32} color="#fff" />}
             </div>
